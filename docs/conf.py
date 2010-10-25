77534adc8e901a23a33dab8ea4ee757f29a73dfb
Heungsub Lee <heung@sublee.kr>
2010-10-25 20:26:55 +0900

    Change the my name in the documentation configuration.

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Flask-Silk'
-copyright = u'2010, Lee Heung-sub'
+copyright = u'2010, Heungsub Lee'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -183,7 +183,7 @@ htmlhelp_basename = 'Flask-Silkdoc'
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
   ('index', 'Flask-Silk.tex', u'Flask-Silk Documentation',
-   u'Lee Heung-sub', 'manual'),
+   u'Heungsub Lee', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -216,5 +216,5 @@ latex_documents = [
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index', 'flask-silk', u'Flask-Silk Documentation',
-     [u'Lee Heung-sub'], 1)
+     [u'Heungsub Lee'], 1)
 ]
