3b5da3bda2fd284936d64ba6f8e646d6d19b561b
Heungsub Lee <sub@subl.ee>
2013-03-25 01:43:32 +0900

    fix issue #3

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from flask import Flask, Module
+from flask import Flask, Blueprint
 
 from flask.ext.silk import Silk
 
@@ -20,18 +20,18 @@ class ModuleTestCase(unittest.TestCase):
 
     def test_urlprefix(self):
         app = Flask(__name__)
-        mod = Module(__name__ + '.foo', url_prefix='/foo')
-        silk = Silk(mod)
-        app.register_module(mod)
+        blu = Blueprint(__name__, __name__, url_prefix='/foo')
+        silk = Silk(blu)
+        app.register_blueprint(blu)
         rv = app.test_client().get('/foo/icons/page_white.png')
         assert 294 == len(rv.data), 'could not found an icon file.'
 
     def test_subdomain(self):
         app = Flask(__name__)
         app.config['SERVER_NAME'] = 'example.org'
-        mod = Module(__name__ + '.foo', subdomain='foo')
-        silk = Silk(mod)
-        app.register_module(mod)
+        blu = Blueprint(__name__, __name__, subdomain='foo')
+        silk = Silk(blu)
+        app.register_blueprint(blu)
         rv = app.test_client().get('/icons/page_white.png',
                                    'http://foo.example.org/')
         assert 294 == len(rv.data), 'could not found an icon file.'
